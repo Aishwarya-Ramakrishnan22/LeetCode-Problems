3c97b99a728c93f1110a69d31948e98d77c49631
Aishwarya-Ramakrishnan22 <91653970+Aishwarya-Ramakrishnan22@users.noreply.github.com>
2023-10-11 15:59:27 +0530

    Time: 60 ms (66.87%), Space: 43.1 MB (31.16%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const hashmap = {}
+    for(let i = 0; i<nums.length;i++){
+        diff = target - nums[i]
+        if (diff in hashmap){
+            return [hashmap[diff],i]
+        }
+        hashmap[nums[i]]=i
+        
+    }
+    
+};
